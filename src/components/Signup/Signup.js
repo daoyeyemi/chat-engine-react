c1723c6e9cd709b6991a4d717d3c19a34a965bcc
daogen <daoyeyemi@aol.com>
2021-04-11 04:04:28 -0700

    Formik components as well as form, field, and error message components

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import "../../App.css";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import validate from "./config";
+import defaultVals from "./config";
 
 function Signup() {
     return (
         <div className="form">
             <h1>Sign up</h1>
-            <Formik>
+            <Formik
+                onSubmit={() => {console.log("Submitting...")} }
+                validateOnMount={true}
+                initialValues={defaultVals}
+                validate={validate}
+            >
                 <Form>
                     <Field type="email" name="email"/>
                     <ErrorMessage name="email" component="div"/>
+                    <Field type="username" name="username"/>
+                    <ErrorMessage name="password" component="div"/>
                     <Field type="password" name="password"/>
                     <ErrorMessage name="password" component="div"/>
+                    <Field type="verifyPassword" name="verifyPassword"/>
+                    <ErrorMessage name="verifyPassword" component="div"/>
                     <button type="submit">Sign up</button>
                 </Form>    
             </ Formik>
--- src/components/Signup/config.js
+++ src/components/Signup/config.js
@@ -11,5 +11,5 @@ export const validate = Yup.object().shape({
     email: Yup.string().email("Invalid email address").required("Required"),
     password: Yup.string().required("Required").min(6, "must be at least six characters"),
     userName: Yup.string().required("Required").matches(/^\S*$/, "Spaces not allowed").min(5, "must be at least six characters"),
-    verifyPassword: Yup.string().required("Required").oneOf([Yup.ref("password"), null], "Passwords do not match"),
+    verifyPassword: Yup.string().required("Required").oneOf([Yup.ref("password"), null], "Passwords do not match")
 });
