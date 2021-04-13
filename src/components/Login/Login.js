b874a978db65ef7a52e5141a9fabe1f6e9a004b3
daogen <daoyeyemi@aol.com>
2021-04-13 01:18:10 -0700

    finished basica functionality of login page and styling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "../../App.css";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { Link } from "react-router-dom";
 
 function Login() {
     
@@ -15,15 +16,20 @@ function Login() {
         password: Yup.string().required("Required")
     });
 
+    const loggingIn = ({email, password, setSubmitting}) => {
+        console.log("Logging In: ", email, password);
+    }
+
     return (
         <div className="auth-form">
             <h1>Login</h1>
             <Formik
-                    // onSubmit={}
+                    onSubmit={loggingIn}
                     validateOnMount={true}
                     initialValues={defaultValues}
                     validationSchema={validationSchema}
             >
+                {({ isValid, isSubmitting }) => (
                 <Form>
                     <label>
                         Email
@@ -35,11 +41,19 @@ function Login() {
                         <Field className="form-control" type="password" name="password"/>
                         <ErrorMessage className="error" name="password" component="div"/>
                     </label> 
+                    <Link to="/signup">
+                    <div className="auth-link-container auth-link">
+                        Don't have an account already?
+                    </div>
+                    </Link>
+                    
+                    <button disabled={isSubmitting === true || isValid === false} type="submit">Sign up</button>
+ 
                 </Form>
-                
+                )}
             </Formik>
         </div>
-        
+                
     )
 }
 
