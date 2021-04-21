b01bd2ed7e2253d840feede534fdf287f53c997f
daogen <daoyeyemi@aol.com>
2021-04-20 22:33:18 -0700

    added some content to ImageUpload component

--- src/components/ImageUpload/index.js
+++ src/components/ImageUpload/index.js
@@ -1,5 +1,18 @@
+import { Modal } from "semantic-ui-react";
+
 export const ImageUpload = () => {
+    const [image, setImage] = useState("");
+    const crop = useRef();
+
     return (
-        
+        <Modal>
+            <Modal.Header>Send this message?</Modal.Header>
+            <Modal.Content>
+
+            </Modal.Content>
+            <Modal.Actions>
+
+            </Modal.Actions>
+        </Modal>
     )
 }
